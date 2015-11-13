7677f63bd3d424af090c25352c0dacbf02859671
cuixinkuan <cui19910119@163.com>
2015-11-13 16:24:51 +0800

    Initial Commit

---
+++ pickerviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pickerviewDemo
+//
+//  Created by admin on 15/11/13.
+//  Copyright © 2015年 CXK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
